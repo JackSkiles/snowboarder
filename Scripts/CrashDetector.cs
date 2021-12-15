46b28e0dcb1d467520b279130392d90866118349
Jack Skiles <jackskiles420@gmail.com>
2021-12-15 18:37:29 -0500

    updated some variables

--- Scripts/CrashDetector.cs
+++ Scripts/CrashDetector.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float ReloadTime = 1f;
+
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Ground")
         {
-            Debug.Log("Bonk!");
+            Invoke("LevelReset", ReloadTime);
         }
+    }
+
+    void LevelReset() {
+        Debug.Log("Bonk!");
+        SceneManager.LoadScene(0);
     }
 }
--- Scripts/FinishLine.cs
+++ Scripts/FinishLine.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float ReloadTime = 1f;
     // Start is called before the first frame update
-    void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Player")
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
         {
-            Debug.Log("You finished");
+            Invoke("LevelReset", ReloadTime);
+
         }
+    }
+    void LevelReset()
+    {
+        SceneManager.LoadScene(0);
     }
 }
